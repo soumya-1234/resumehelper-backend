e25c021105a91598033d4d925b8bd83d44167e3f
soumya-1234 <soumya977761@gmail.com>
2022-11-28 23:13:38 +0530

    CORS enable

--- index.js
+++ index.js
@@ -17,7 +17,17 @@ const Jobdet=require("./models/Jobdet");
 
 
 const app=express();
+
+app.use("/",(req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET, POST , PUT , DELETE');
+  res.setHeader('Access-Control-Allow-Headers','Content-type, Authorization');
+  next();
+});
+
+
 app.use(express.json());
+
 
 //students
 
